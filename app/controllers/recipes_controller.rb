cffb70ca2d171fd67ba2aee3ad8d12e7f74deb66
Marisa You <marisayou2017@u.northwestern.edu>
2020-11-04 13:31:22 -0800

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,8 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def index
@@ -9,8 +11,23 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(
+      :title, 
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
